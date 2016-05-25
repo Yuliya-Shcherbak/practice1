2f99fece1082ad4cbee1e06192291afd1f66d424
Julia <shcherbak.yuliya@gmail.com>
2016-05-25 18:48:51 +0300

    4: class SomeClass added

--- SomeConsoleApp/SomeConsoleApp/Program.cs
+++ SomeConsoleApp/SomeConsoleApp/Program.cs
@@ -9,6 +9,24 @@ namespace SomeConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SomeClass.SayHello());
+            Console.WriteLine("New number is: {0}", SomeClass.PrintSomeNumber());
+            Console.ReadKey();
+        }
+    }
+
+    class SomeClass
+    {
+        public static int PrintSomeNumber()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 1000);
+            return num;
+        }
+
+        public static string SayHello()
+        {
+            return "Hello World!";
         }
     }
 }
